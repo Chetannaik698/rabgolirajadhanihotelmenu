8feebac4f829129690f26949383dda4f5235edee
Chetan Naik <chetannaik11145@gmail.com>
2025-03-20 23:00:14 +0530

    latest

--- src/components/Starters_Snacks/Starters_Snacks_Page.jsx
+++ src/components/Starters_Snacks/Starters_Snacks_Page.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Snacks from './Snacks'
-import SoupsStaters from './soupsStaters'
+import SoupsStaters from './SoupsStaters'
 import StatersBoneLess from './StatersBoneLess'
 import Staters from './Staters'
 import EggStaters from './EggStaters'
